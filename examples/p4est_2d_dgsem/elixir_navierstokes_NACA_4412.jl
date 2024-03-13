03300ee0a9199da104810806f185397cc1e284c7
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-03-13 11:24:49 +0100

    NACA 4412 example

---
+++ examples/p4est_2d_dgsem/elixir_navierstokes_NACA_4412.jl
@@ -0,0 +1,131 @@
+using OrdinaryDiffEq
+using Trixi
+
+###############################################################################
+# semidiscretization of the compressible Euler equations
+
+prandtl_number() = 0.72
+mu() = 1.81 * 1e-5
+
+equations = CompressibleEulerEquations2D(1.4)
+equations_parabolic = CompressibleNavierStokesDiffusion2D(equations, mu = mu(),
+                                                          Prandtl = prandtl_number(),
+                                                          gradient_variables = GradientVariablesPrimitive())
+
+@inline function initial_condition_freestream(x, t, equations)
+  # set the freestream flow parameters
+  rho_freestream = 1.4
+  v1 = 0.2
+  v2 = 0.0
+  p_freestream = 1.0
+
+  prim = SVector(rho_freestream, v1, v2, p_freestream)
+  return prim2cons(prim, equations)
+end
+
+initial_condition = initial_condition_freestream
+
+polydeg = 2
+volume_flux = flux_ranocha
+solver = DGSEM(polydeg = polydeg, surface_flux = flux_lax_friedrichs)
+
+###############################################################################
+# Get the uncurved mesh from a file (downloads the file if not available locally)
+
+path = "/home/daniel/ownCloud - Döhring, Daniel (1MH1D4@rwth-aachen.de)@rwth-aachen.sciebo.de/Job/Doktorand/Content/Meshes/NASA_meshes/"
+mesh_file = path * "NACA0012/n0012family_1_7_2D_unique.inp"
+mesh_file = path * "NACA4412/NACA4412_1_2D_unique.inp"
+
+boundary_symbols = [:b2_symmetry_y_strong,
+                    :b4_farfield_riem, :b5_farfield_riem, :b7_farfield_riem, :b6_viscous_solid, :b8_to_stitch_a]
+
+mesh = P4estMesh{2}(mesh_file, polydeg = polydeg, boundary_symbols = boundary_symbols)
+
+# Boundary conditions for free-stream testing
+boundary_condition_free_stream = BoundaryConditionDirichlet(initial_condition)
+
+boundary_conditions = Dict(:b2_symmetry_y_strong => boundary_condition_free_stream,
+                           :b4_farfield_riem => boundary_condition_free_stream,
+                           :b5_farfield_riem => boundary_condition_free_stream,
+                           :b7_farfield_riem => boundary_condition_free_stream,
+                           :b6_viscous_solid => boundary_condition_slip_wall,
+                           :b8_to_stitch_a => boundary_condition_free_stream)
+
+velocity_bc_airfoil = NoSlip((x, t, equations) -> SVector(0.0, 0.0))
+heat_bc = Adiabatic((x, t, equations) -> 0.0)
+boundary_condition_airfoil = BoundaryConditionNavierStokesWall(velocity_bc_airfoil, heat_bc)
+
+boundary_conditions_parabolic = Dict(:b2_symmetry_y_strong => boundary_condition_free_stream,
+                                     :b4_farfield_riem => boundary_condition_free_stream,
+                                     :b5_farfield_riem => boundary_condition_free_stream,
+                                     :b7_farfield_riem => boundary_condition_free_stream,
+                                     :b6_viscous_solid => boundary_condition_airfoil,
+                                     :b8_to_stitch_a => boundary_condition_free_stream)
+
+semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabolic),
+                                             initial_condition, solver;
+                                             boundary_conditions = (boundary_conditions,
+                                                                    boundary_conditions_parabolic))
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+tspan = (0.0, 0.02)
+ode = semidiscretize(semi, tspan) # ODE Integrators
+#ode = semidiscretize(semi, tspan; split_form = false) # PERK
+
+summary_callback = SummaryCallback()
+
+analysis_interval = 10^5
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
+
+alive_callback = AliveCallback(alive_interval = 200)
+
+stepsize_callback = StepsizeCallback(cfl = 0.4) # CarpenterKennedy2N54
+
+#stepsize_callback = StepsizeCallback(cfl = 1.5) # PERK4
+
+callbacks = CallbackSet(summary_callback,
+                        analysis_callback,
+                        alive_callback,
+                        stepsize_callback)
+
+###############################################################################
+# run the simulation
+
+#=
+dtRatios = [0.249748130716557,
+            0.229743135233184,
+            0.148737624222123,
+            0.106506037112321,
+            0.092058178144161,
+            0.066218481684170,
+            0.047412460769779,
+            0.027795091314087] / 0.249748130716557
+
+#Stages = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5]
+Stages = [16, 15, 11, 9, 8, 7, 6, 5]
+ode_algorithm = PERK4_Multi(Stages, "/home/daniel/git/MA/EigenspectraGeneration/SD7003/", dtRatios)
+
+#ode_algorithm = PERK4(14, "/home/daniel/git/MA/EigenspectraGeneration/SD7003/")
+
+sol = Trixi.solve(ode, ode_algorithm,
+                  dt = 42.0,
+                  save_everystep=false, callback=callbacks);
+=#
+
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false, thread = OrdinaryDiffEq.True()),
+            dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+            save_everystep = false, callback = callbacks);
+
+summary_callback() # print the timer summary
+
+
+using Plots
+
+pd = PlotData2D(sol)
+plot(sol)
+
+plot(pd["rho"])
+
+plot(getmesh(pd), xlim = [-1, 2], ylim = [-0.5, 0.5])
--- src/time_integration/methods_PERK4Multi.jl
+++ src/time_integration/methods_PERK4Multi.jl
@@ -671,8 +671,8 @@ function solve!(integrator::PERK4_Multi_Integrator)
     @trixi_timeit timer() "Paired Explicit Runge-Kutta ODE integration step" begin
       
       # k1: Evaluated on entire domain / all levels
-      #integrator.f(integrator.du, integrator.u, prob.p, integrator.t, integrator.du_ode_hyp)
-      integrator.f(integrator.du, integrator.u, prob.p, integrator.t)
+      integrator.f(integrator.du, integrator.u, prob.p, integrator.t, integrator.du_ode_hyp)
+      #integrator.f(integrator.du, integrator.u, prob.p, integrator.t)
 
       @threaded for i in eachindex(integrator.du)
         integrator.k1[i] = integrator.du[i] * integrator.dt
@@ -691,7 +691,7 @@ function solve!(integrator::PERK4_Multi_Integrator)
       =#
 
       # CARE: This does not work if we have only one method but more than one grid level
-      #=
+      
       integrator.f(integrator.du, integrator.u_tmp, prob.p, integrator.t_stage, 
                    integrator.level_info_elements_acc[1],
                    integrator.level_info_interfaces_acc[1],
@@ -700,15 +700,15 @@ function solve!(integrator::PERK4_Multi_Integrator)
                    integrator.level_info_mortars_acc[1],
                    integrator.level_u_indices_elements, 1,
                    integrator.du_ode_hyp)
-      =#
       
+      #=
       integrator.f(integrator.du, integrator.u_tmp, prob.p, integrator.t_stage, 
                    integrator.level_info_elements_acc[1],
                    integrator.level_info_interfaces_acc[1],
                    integrator.level_info_boundaries_acc[1],
                    integrator.level_info_boundaries_orientation_acc[1],
                    integrator.level_info_mortars_acc[1])
-      
+      =#
 
       # Update finest level only
       @threaded for u_ind in integrator.level_u_indices_elements[1]
@@ -766,7 +766,7 @@ function solve!(integrator::PERK4_Multi_Integrator)
         end
         =#
       
-        #=
+        
         # Joint RHS evaluation with all elements sharing this timestep
         integrator.f(integrator.du, integrator.u_tmp, prob.p, integrator.t_stage, 
                      integrator.level_info_elements_acc[integrator.coarsest_lvl],
@@ -776,15 +776,15 @@ function solve!(integrator::PERK4_Multi_Integrator)
                      integrator.level_info_mortars_acc[integrator.coarsest_lvl],
                      integrator.level_u_indices_elements, integrator.coarsest_lvl,
                      integrator.du_ode_hyp)
-        =#
         
+        #=
         integrator.f(integrator.du, integrator.u_tmp, prob.p, integrator.t_stage, 
                      integrator.level_info_elements_acc[integrator.coarsest_lvl],
                      integrator.level_info_interfaces_acc[integrator.coarsest_lvl],
                      integrator.level_info_boundaries_acc[integrator.coarsest_lvl],
                      integrator.level_info_boundaries_orientation_acc[integrator.coarsest_lvl],
                      integrator.level_info_mortars_acc[integrator.coarsest_lvl])
-        
+        =#
 
         # Update k_higher of relevant levels
         for level in 1:integrator.coarsest_lvl          
@@ -802,8 +802,8 @@ function solve!(integrator::PERK4_Multi_Integrator)
         end
         integrator.t_stage = integrator.t + alg.c[alg.NumStages - 3 + stage] * integrator.dt
 
-        #integrator.f(integrator.du, integrator.u_tmp, prob.p, integrator.t_stage, integrator.du_ode_hyp)
-        integrator.f(integrator.du, integrator.u_tmp, prob.p, integrator.t_stage)
+        integrator.f(integrator.du, integrator.u_tmp, prob.p, integrator.t_stage, integrator.du_ode_hyp)
+        #integrator.f(integrator.du, integrator.u_tmp, prob.p, integrator.t_stage)
 
         @threaded for u_ind in eachindex(integrator.u)
           integrator.k_higher[u_ind] = integrator.du[u_ind] * integrator.dt
