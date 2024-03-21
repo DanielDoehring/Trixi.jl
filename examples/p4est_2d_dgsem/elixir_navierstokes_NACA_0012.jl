ffd1903d7700c5463db356da71223643872af9de
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-03-21 14:19:13 +0100

    airfoil

--- examples/p4est_2d_dgsem/elixir_navierstokes_NACA_0012.jl
+++ examples/p4est_2d_dgsem/elixir_navierstokes_NACA_0012.jl
@@ -25,9 +25,11 @@ T_Ref_K = T_ref_R / 1.8 # 300 K
 R_spec_air = 287.052874 # TODO: Not sure if this is correct, leads to high density and pressure
 
 #u_Ref = Ma * sqrt(gamma * R_spec_air * T_Ref_K)
-u_Ref = Ma * sqrt(gamma * T_Ref_K)
+#u_Ref = Ma * sqrt(gamma * T_Ref_K)
+u_Ref = 52.08
 
-angle = 10 * pi/180
+angle = 5 * pi/180
+#angle = 0
 
 mu_0 = 1.716 * 10^-5
 mu(T) = mu_0 * (T/T0)^(3/2) * (T0 + S) / (T + S)
@@ -36,9 +38,11 @@ mu_Ref = mu(T_Ref_K)
 L = 1.0 # Airfoil chord length
 rho_Ref = Re * mu_Ref / (u_Ref * L)
 #rho_Ref = Re * mu_Ref / (u_Ref * L * sqrt(R_spec_air))
+rho_Ref = 1.1767527315296566
 
 #p_Ref = rho_Ref * R_spec_air * T_Ref_K
-p_Ref = rho_Ref * T_Ref_K
+#p_Ref = rho_Ref * T_Ref_K
+p_Ref = 101325
 
 prim = SVector(rho_Ref, u_Ref * cos(angle), u_Ref * sin(angle), p_Ref)
 cons = prim2cons(prim, CompressibleEulerEquations2D(gamma))
@@ -46,11 +50,8 @@ p = (gamma - 1) * (cons[4] - 0.5 * (cons[1]^2 + cons[2]^2) / cons[1])
 T = p / cons[1]
 
 prandtl_number() = 0.72
-mu() = 1.8459162511975808e-5
-
-# Check values
-Ma_check = u_Ref / sqrt(gamma * T_Ref_K)
-Re_check = rho_Ref * u_Ref * L / mu_Ref
+#mu() = 1.8459162511975808e-5
+mu() = 1.0214213709677419e-5
 
 equations = CompressibleEulerEquations2D(gamma)
 equations_parabolic = CompressibleNavierStokesDiffusion2D(equations, mu = mu(),
@@ -66,10 +67,10 @@ equations_parabolic = CompressibleNavierStokesDiffusion2D(equations, mu = mu(),
   p_freestream = p_Ref
   =#
 
-  rho_freestream = 2.126504909489584
-  v1 = 3.0273829677154183
-  v2 = 0.5338092981454919
-  p_freestream = 637.9514728468752
+  rho_freestream = 1.9769585253456223
+  v1 = 51.88181987661811
+  v2 = 4.539071082298038
+  p_freestream = 101325
 
   prim = SVector(rho_freestream, v1, v2, p_freestream)
   return prim2cons(prim, equations)
@@ -78,7 +79,12 @@ end
 initial_condition = initial_condition_freestream
 
 polydeg = 3
-solver = DGSEM(polydeg = polydeg, surface_flux = flux_lax_friedrichs)
+
+surface_flux = flux_hllc
+volume_flux = flux_ranocha
+
+solver = DGSEM(polydeg = polydeg, surface_flux = surface_flux,
+               volume_integral = VolumeIntegralFluxDifferencing(volume_flux))
 
 ###############################################################################
 # Get the uncurved mesh from a file (downloads the file if not available locally)
@@ -124,15 +130,14 @@ semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabol
 # ODE solvers, callbacks etc.
 
 t_c = L / u_Ref
-#tspan = (0.0, 100 * t_c)
-tspan = (0.0, 1e-4)
+tspan = (0.0, 1 * t_c)
 
 ode = semidiscretize(semi, tspan) # ODE Integrators
 ode = semidiscretize(semi, tspan; split_form = false) # PERK
 
 summary_callback = SummaryCallback()
 
-analysis_interval = 500
+analysis_interval = 2000
 
 aoa() = angle
 rho_inf() = rho_Ref
@@ -156,14 +161,16 @@ analysis_callback = AnalysisCallback(semi, interval = analysis_interval,
                                      analysis_integrals = (drag_coefficient,
                                                            lift_coefficient))
 
-alive_callback = AliveCallback(alive_interval = 200)
+alive_callback = AliveCallback(alive_interval = 1000)
 
-stepsize_callback = StepsizeCallback(cfl = 0.4) # CarpenterKennedy2N54
+stepsize_callback = StepsizeCallback(cfl = 4.0) # PERK4, Family 1 
 
-stepsize_callback = StepsizeCallback(cfl = 2.0) # PERK4
+save_restart = SaveRestartCallback(interval = 5e4,
+                                   save_final_restart = true)
 
 callbacks = CallbackSet(summary_callback,
                         analysis_callback,
+			                  save_restart,
                         alive_callback,
                         stepsize_callback)
 
@@ -198,7 +205,7 @@ sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false, thread = Ord
 
 summary_callback() # print the timer summary
 
-
+#=
 using Plots
 
 pd = PlotData2D(sol)
@@ -207,3 +214,4 @@ plot(sol)
 plot(pd["rho"])
 
 plot(getmesh(pd), xlim = [-1, 2], ylim = [-0.5, 0.5])
+=#
