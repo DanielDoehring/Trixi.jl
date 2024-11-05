4e3928a328209c6a56e5b752b0409b0042cac869
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-11-05 17:07:20 +0100

    First version perk4

---
+++ examples/structured_2d_dgsem/elixir_euler_vortex.jl
@@ -0,0 +1,97 @@
+
+using OrdinaryDiffEq
+using Trixi
+
+# Ratio of specific heats
+gamma = 1.4
+equations = CompressibleEulerEquations2D(gamma)
+
+solver = DGSEM(polydeg=3, surface_flux=flux_hllc)
+
+"""
+    initial_condition_isentropic_vortex(x, t, equations::CompressibleEulerEquations2D)
+
+The classical isentropic vortex test case as presented in 
+https://spectrum.library.concordia.ca/id/eprint/985444/1/Paired-explicit-Runge-Kutta-schemes-for-stiff-sy_2019_Journal-of-Computation.pdf
+"""
+function initial_condition_isentropic_vortex(x, t, equations::CompressibleEulerEquations2D)
+  # Evaluate error after full domain traversion
+  if t == T_end
+    t = 0
+  end
+
+  # initial center of the vortex
+  inicenter = SVector(0.0, 0.0)
+  # strength of the vortex
+  S = 13.5
+  # Radius of vortex
+  R = 1.5
+  # Free-stream Mach 
+  M = 0.4
+  # base flow
+  v1 = 1.0
+  v2 = 1.0
+  vel = SVector(v1, v2)
+
+  cent = inicenter + vel*t      # advection of center
+  cent = x - cent               # distance to centerpoint
+  cent = SVector(cent[2], -cent[1])
+  r2 = cent[1]^2 + cent[2]^2
+
+  f = (1 - r2) / (2 * R^2)
+
+  rho = (1 - (S*M/pi)^2 * (gamma - 1)*exp(2*f) / 8)^(1/(gamma - 1))
+
+  du = S/(2*π*R)*exp(f) # vel. perturbation
+  vel = vel + du*cent
+  v1, v2 = vel
+
+  p = rho^gamma / (gamma * M^2)
+  prim = SVector(rho, v1, v2, p)
+  return prim2cons(prim, equations)
+end
+initial_condition = initial_condition_isentropic_vortex
+
+EdgeLength = 20
+
+N_passes = 1
+T_end = EdgeLength * N_passes
+tspan = (0.0, T_end)
+
+coordinates_min = (-EdgeLength/2, -EdgeLength/2)
+coordinates_max = ( EdgeLength/2,  EdgeLength/2)
+
+cells_per_dimension = (32, 32)
+mesh = StructuredMesh(cells_per_dimension, coordinates_min, coordinates_max)
+
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver)
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+ode = semidiscretize(semi, tspan)
+
+summary_callback = SummaryCallback()
+
+analysis_interval = 100
+analysis_callback = AnalysisCallback(semi, interval=analysis_interval,
+                                     analysis_integrals = (entropy,))
+
+# Note quite large CFL number
+stepsize_callback = StepsizeCallback(cfl = 9.1)
+
+callbacks = CallbackSet(summary_callback,
+                        stepsize_callback,
+                        analysis_callback)                
+
+###############################################################################
+# run the simulation
+
+Stages = 19
+ode_algorithm = Trixi.PairedExplicitRK4(Stages, "/home/daniel/git/MA/EigenspectraGeneration/PERK4/IsentropicVortex_c1/")
+
+sol = Trixi.solve(ode, ode_algorithm,
+                  dt = 42.0, # Not used
+                  save_everystep=false, callback=callbacks);
+
+summary_callback() # print the timer summary
