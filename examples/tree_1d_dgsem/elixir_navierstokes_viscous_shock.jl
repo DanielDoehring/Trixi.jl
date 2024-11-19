1d46fd833d55d026e9c9d3c36855eb3c3deab182
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-11-19 11:02:58 +0100

    fmt

--- examples/tree_1d_dgsem/elixir_navierstokes_viscous_shock.jl
+++ examples/tree_1d_dgsem/elixir_navierstokes_viscous_shock.jl
@@ -30,12 +30,14 @@ using Trixi
 #   [DOI: 10.1016/j.ijnonlinmec.2017.07.003](https://doi.org/10.1016/j.ijnonlinmec.2017.07.003)
 
 ### Fixed parameters ###
-alpha = 0.5 # Special value for which nonlinear solver can be omitted
+# Special value for which nonlinear solver can be omitted
+# Corresponds essentially to fixing the Mach number
+alpha = 0.5
 # We want kappa = cp * mu = mu_bar to ensure constant enthalpy
 prandtl_number() = 1
 
 ### Free choices: ###
-gamma = 5/3
+gamma = 5 / 3
 rho_0 = 1
 mu() = 0.1
 v = 1 # Shock speed
@@ -49,22 +51,32 @@ c_0 = v / Ma # Speed of sound ahead of the shock
 # From constant enthalpy condition
 p_0 = c_0^2 * rho_0 / gamma
 
-l = mu()/(rho_0 * v) * 2 * gamma / (gamma + 1) # Appropriate lenght scale
+l = mu() / (rho_0 * v) * 2 * gamma / (gamma + 1) # Appropriate lenght scale
 
 # Helper function for coordinate transformation. See eq. (33) in Margolin et al. (2017)
 chi_of_y(y) = 2 * exp(y / (2 * l))
 
+"""
+    initial_condition_viscous_shock(x, t, equations)
+
+Classic 1D viscous shock wave problem with analytical solution 
+for a special value of the Prandtl number.
+The version implemented here is described in
+- L. G. Margolin, J. M. Reisner, P. M. Jordan (2017)
+  Entropy in self-similar shock profiles
+  [DOI: 10.1016/j.ijnonlinmec.2017.07.003](https://doi.org/10.1016/j.ijnonlinmec.2017.07.003)
+"""
 function initial_condition_viscous_shock(x, t, equations)
-  y = x[1] - v * t
+    y = x[1] - v * t
 
-  chi = chi_of_y(y)
-  w = 1 + 1 / (2 * chi^2) * (1 - sqrt(1 + 2 * chi^2))
+    chi = chi_of_y(y)
+    w = 1 + 1 / (2 * chi^2) * (1 - sqrt(1 + 2 * chi^2))
 
-  rho = rho_0 / w
-  u = v * (1 - w)
-  p = p_0 * 1/w * (1 + (gamma - 1)/2 * Ma^2 * (1 - w^2))
+    rho = rho_0 / w
+    u = v * (1 - w)
+    p = p_0 * 1 / w * (1 + (gamma - 1) / 2 * Ma^2 * (1 - w^2))
 
-  return prim2cons(SVector(rho, u, p), equations)
+    return prim2cons(SVector(rho, u, p), equations)
 end
 
 equations = CompressibleEulerEquations1D(gamma)
@@ -74,11 +86,11 @@ equations_parabolic = CompressibleNavierStokesDiffusion1D(equations, mu = mu(),
 
 solver = DGSEM(polydeg = 3, surface_flux = flux_hlle)
 
-coordinates_min = -domain_length/2
-coordinates_max = domain_length/2
+coordinates_min = -domain_length / 2
+coordinates_max = domain_length / 2
 
 mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level = 6,
+                initial_refinement_level = 4,
                 periodicity = false,
                 n_cells_max = 30_000)
 
@@ -88,7 +100,7 @@ initial_condition = initial_condition_viscous_shock
 
 # Prescribe pure influx based on initial conditions
 function boundary_condition_inflow(u_inner, orientation, normal_direction, x, t,
-                                    surface_flux_function, equations)
+                                   surface_flux_function, equations)
     u_cons = initial_condition(x, t, equations)
     flux = Trixi.flux(u_cons, normal_direction, equations)
 
@@ -111,7 +123,7 @@ boundary_conditions = (; x_neg = boundary_condition_inflow,
 # For the viscous BCs, we use the known analytical solution
 velocity_bc = NoSlip((x, t, equations) -> Trixi.velocity(initial_condition(x,
                                                                            t,
-                                                                           equations), 
+                                                                           equations),
                                                          equations))
 
 heat_bc = Isothermal((x, t, equations) -> Trixi.temperature(initial_condition(x,
@@ -143,18 +155,13 @@ alive_callback = AliveCallback(alive_interval = 100)
 analysis_interval = 1000
 analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
 
-visualization = VisualizationCallback(interval=10, plot_data_creator=PlotData1D)
-
-callbacks = CallbackSet(summary_callback, 
-                        alive_callback, 
-                        #visualization,
-                        analysis_callback)
+callbacks = CallbackSet(summary_callback, alive_callback, analysis_callback)
 
 ###############################################################################
 # run the simulation
 
 time_int_tol = 1e-8
-sol = solve(ode, RDPK3SpFSAL49(); abstol = time_int_tol, reltol = time_int_tol, dt = 1e-5,
-            ode_default_options()..., callback = callbacks)
+sol = solve(ode, RDPK3SpFSAL49(); abstol = time_int_tol, reltol = time_int_tol,
+            dt = 1e-5, ode_default_options()..., callback = callbacks)
 
 summary_callback() # print the timer summary
