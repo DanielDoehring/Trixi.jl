be386d83a7c802a44a9b7455e1bce0c14ba2ab0d
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-11-19 10:54:04 +0100

    Get viscous shock to work

--- examples/tree_1d_dgsem/elixir_navierstokes_viscous_shock.jl
+++ examples/tree_1d_dgsem/elixir_navierstokes_viscous_shock.jl
@@ -1,75 +1,70 @@
-using OrdinaryDiffEq, Plots
+using OrdinaryDiffEq
 using Trixi
 
 ###############################################################################
 # semidiscretization of the ideal compressible Navier-Stokes equations
 
-# TODO: Reference for Becker-Morduchow-Libby solution
+# This is the classic 1D viscous shock wave problem with analytical solution 
+# for a special value of the Prandtl number.
+# The original references are:
+#
+# - R. Becker (1922)
+#   Stoßwelle und Detonation.
+#   [DOI: 10.1007/BF01329605](https://doi.org/10.1007/BF01329605)
+#
+#   English translations:
+#   Impact waves and detonation. Part I.
+#   https://ntrs.nasa.gov/api/citations/19930090862/downloads/19930090862.pdf
+#
+#   Impact waves and detonation. Part II.
+#   https://ntrs.nasa.gov/api/citations/19930090863/downloads/19930090863.pdf
+#
+# - M. Morduchow, P. A. Libby (1949)
+#   On a Complete Solution of the One-Dimensional Flow Equations 
+#   of a Viscous, Head-Conducting, Compressible Gas
+#   [DOI: 10.2514/8.11882](https://doi.org/10.2514/8.11882)
+#
+# The particular problem considered here is described in
+# - L. G. Margolin, J. M. Reisner, P. M. Jordan (2017) 
+#   Entropy in self-similar shock profiles
+#   [DOI: 10.1016/j.ijnonlinmec.2017.07.003](https://doi.org/10.1016/j.ijnonlinmec.2017.07.003)
 
-# "Physics"
-gamma = 1.4
-prandtl_number() = 3/4 # Strictly required for this testcase!
+### Fixed parameters ###
+alpha = 0.5 # Special value for which nonlinear solver can be omitted
+# We want kappa = cp * mu = mu_bar to ensure constant enthalpy
+prandtl_number() = 1
 
-# "Choices"
-u_0 = 1.0
-D = u_0
-
-rho_0 = 1.0
-# p_0 = 1.0
-p_0 = rho_0^gamma
-
-Ma_0 = 2.0
-c_0 = u_0 / Ma_0 # Not needed at the moment
-
-eta_1 = (gamma - 1)/(gamma + 1) + 2/((gamma + 1) * Ma_0^2)
-
+### Free choices: ###
+gamma = 5/3
+rho_0 = 1
 mu() = 0.1
+v = 1 # Shock speed
 
-# This is valid for constant viscosity mu
-function xi_of_x(x)
-  3 * (gamma + 1) / (8 * gamma) * rho_0 * D/mu() * x
-end
+domain_length = 5.0
 
-function eta_root_of_x(eta, x)
-  xi = xi_of_x(x)
-  eta - 1 + ((1 - eta_1)/2)^(1 - eta_1) * exp((1 - eta_1) * xi) * (eta - eta_1)^eta_1
-end
+### Derived quantities ###
+Ma = 2 / sqrt(3 - gamma) # Mach number for alpha = 0.5
+c_0 = v / Ma # Speed of sound ahead of the shock
 
-function eta_bisection(eta_0, eta_1, x, eta_tol=1e-15)
-  @assert eta_root_of_x(eta_0, x) * eta_root_of_x(eta_1, x) < 0
+# From constant enthalpy condition
+p_0 = c_0^2 * rho_0 / gamma
 
-  eta_max = eta_0
-  eta_min = eta_1
+l = mu()/(rho_0 * v) * 2 * gamma / (gamma + 1) # Appropriate lenght scale
 
-  eta = (eta_max + eta_min) / 2
-
-  while eta_max - eta_min > eta_tol
-    if eta_root_of_x(eta, x) < 0
-      eta_min = eta
-    else
-      eta_max = eta
-    end
-
-    eta = (eta_max + eta_min) / 2
-  end
-
-  return eta
-end
-
-Length = 2.0
-coordinates_min = -Length/2
-coordinates_max = Length/2
+# Helper function for coordinate transformation. See eq. (33) in Margolin et al. (2017)
+chi_of_y(y) = 2 * exp(y / (2 * l))
 
 function initial_condition_viscous_shock(x, t, equations)
-  eta_sol = eta_bisection(1.0, eta_1, x[1])
+  y = x[1] - v * t
 
-  rho = rho_0 / eta_sol
+  chi = chi_of_y(y)
+  w = 1 + 1 / (2 * chi^2) * (1 - sqrt(1 + 2 * chi^2))
 
-  v = eta_sol * D
-  
-  p = p_0 * 1/eta_sol * (1 + (gamma - 1)/2 * Ma_0^2 * (1 - eta_sol^2))
+  rho = rho_0 / w
+  u = v * (1 - w)
+  p = p_0 * 1/w * (1 + (gamma - 1)/2 * Ma^2 * (1 - w^2))
 
-  return prim2cons(SVector(rho, v, p), equations)
+  return prim2cons(SVector(rho, u, p), equations)
 end
 
 equations = CompressibleEulerEquations1D(gamma)
@@ -77,28 +72,47 @@ equations_parabolic = CompressibleNavierStokesDiffusion1D(equations, mu = mu(),
                                                           Prandtl = prandtl_number(),
                                                           gradient_variables = GradientVariablesEntropy())
 
-solver = DGSEM(polydeg = 3, surface_flux = flux_hlle,
-               volume_integral = VolumeIntegralWeakForm())
+solver = DGSEM(polydeg = 3, surface_flux = flux_hlle)
 
-# Create a uniformly refined mesh with periodic boundaries
+coordinates_min = -domain_length/2
+coordinates_max = domain_length/2
+
 mesh = TreeMesh(coordinates_min, coordinates_max,
                 initial_refinement_level = 6,
                 periodicity = false,
-                n_cells_max = 30_000) # set maximum capacity of tree data structure
+                n_cells_max = 30_000)
 
 initial_condition = initial_condition_viscous_shock
 
-boundary_condition_dirichlet = BoundaryConditionDirichlet(initial_condition)
-# define inviscid boundary conditions
+### Inviscid boundary conditions ###
 
-boundary_conditions = (; x_neg = boundary_condition_dirichlet,
-                       x_pos = boundary_condition_dirichlet)
+# Prescribe pure influx based on initial conditions
+function boundary_condition_inflow(u_inner, orientation, normal_direction, x, t,
+                                    surface_flux_function, equations)
+    u_cons = initial_condition(x, t, equations)
+    flux = Trixi.flux(u_cons, normal_direction, equations)
 
+    return flux
+end
+
+# Completely free outflow
+function boundary_condition_outflow(u_inner, orientation, normal_direction, x, t,
+                                    surface_flux_function, equations)
+    # Calculate the boundary flux entirely from the internal solution state
+    flux = Trixi.flux(u_inner, normal_direction, equations)
+
+    return flux
+end
+
+boundary_conditions = (; x_neg = boundary_condition_inflow,
+                       x_pos = boundary_condition_outflow)
+
+### Viscous boundary conditions ###
+# For the viscous BCs, we use the known analytical solution
 velocity_bc = NoSlip((x, t, equations) -> Trixi.velocity(initial_condition(x,
                                                                            t,
                                                                            equations), 
                                                          equations))
-
 
 heat_bc = Isothermal((x, t, equations) -> Trixi.temperature(initial_condition(x,
                                                                               t,
@@ -107,7 +121,6 @@ heat_bc = Isothermal((x, t, equations) -> Trixi.temperature(initial_condition(x,
 
 boundary_condition_parabolic = BoundaryConditionNavierStokesWall(velocity_bc, heat_bc)
 
-# define viscous boundary conditions
 boundary_conditions_parabolic = (; x_neg = boundary_condition_parabolic,
                                  x_pos = boundary_condition_parabolic)
 
@@ -120,7 +133,7 @@ semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabol
 # ODE solvers, callbacks etc.
 
 # Create ODE problem with time span `tspan`
-tspan = (0.0, 0.1)
+tspan = (0.0, 0.5)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
@@ -143,4 +156,5 @@ callbacks = CallbackSet(summary_callback,
 time_int_tol = 1e-8
 sol = solve(ode, RDPK3SpFSAL49(); abstol = time_int_tol, reltol = time_int_tol, dt = 1e-5,
             ode_default_options()..., callback = callbacks)
+
 summary_callback() # print the timer summary
