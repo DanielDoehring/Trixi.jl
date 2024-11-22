1fb94021f982d219d0be1085fff90f158996fc2f
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-11-22 11:15:26 +0100

    conv test

--- examples/tree_1d_dgsem/elixir_navierstokes_viscous_shock.jl
+++ examples/tree_1d_dgsem/elixir_navierstokes_viscous_shock.jl
@@ -1,9 +1,6 @@
 using OrdinaryDiffEq
 using Trixi
 
-###############################################################################
-# semidiscretization of the ideal compressible Navier-Stokes equations
-
 # This is the classic 1D viscous shock wave problem with analytical solution 
 # for a special value of the Prandtl number.
 # The original references are:
@@ -15,7 +12,6 @@ using Trixi
 #   English translations:
 #   Impact waves and detonation. Part I.
 #   https://ntrs.nasa.gov/api/citations/19930090862/downloads/19930090862.pdf
-#
 #   Impact waves and detonation. Part II.
 #   https://ntrs.nasa.gov/api/citations/19930090863/downloads/19930090863.pdf
 #
@@ -24,12 +20,14 @@ using Trixi
 #   of a Viscous, Head-Conducting, Compressible Gas
 #   [DOI: 10.2514/8.11882](https://doi.org/10.2514/8.11882)
 #
+#
 # The particular problem considered here is described in
 # - L. G. Margolin, J. M. Reisner, P. M. Jordan (2017) 
 #   Entropy in self-similar shock profiles
 #   [DOI: 10.1016/j.ijnonlinmec.2017.07.003](https://doi.org/10.1016/j.ijnonlinmec.2017.07.003)
 
 ### Fixed parameters ###
+
 # Special value for which nonlinear solver can be omitted
 # Corresponds essentially to fixing the Mach number
 alpha = 0.5
@@ -37,24 +35,25 @@ alpha = 0.5
 prandtl_number() = 1
 
 ### Free choices: ###
-gamma = 5 / 3
-rho_0 = 1
-mu() = 0.1
-v = 1 # Shock speed
+gamma() = 5 / 3
 
-domain_length = 5.0
+mu() = 0.15
+mu_bar() = mu() / (gamma() - 1)
+
+rho_0() = 1
+v() = 1 # Shock speed
+
+domain_length = 4.0
 
 ### Derived quantities ###
-Ma = 2 / sqrt(3 - gamma) # Mach number for alpha = 0.5
-c_0 = v / Ma # Speed of sound ahead of the shock
+
+Ma() = 2 / sqrt(3 - gamma()) # Mach number for alpha = 0.5
+c_0() = v() / Ma() # Speed of sound ahead of the shock
 
 # From constant enthalpy condition
-p_0 = c_0^2 * rho_0 / gamma
+p_0() = c_0()^2 * rho_0() / gamma()
 
-l = mu() / (rho_0 * v) * 2 * gamma / (gamma + 1) # Appropriate lenght scale
-
-# Helper function for coordinate transformation. See eq. (33) in Margolin et al. (2017)
-chi_of_y(y) = 2 * exp(y / (2 * l))
+l() = mu_bar() / (rho_0() * v()) * 2 * gamma() / (gamma() + 1) # Appropriate length scale
 
 """
     initial_condition_viscous_shock(x, t, equations)
@@ -67,51 +66,63 @@ The version implemented here is described in
   [DOI: 10.1016/j.ijnonlinmec.2017.07.003](https://doi.org/10.1016/j.ijnonlinmec.2017.07.003)
 """
 function initial_condition_viscous_shock(x, t, equations)
-    y = x[1] - v * t
+    y = x[1] - v() * t # Translated coordinate
 
-    chi = chi_of_y(y)
+    # Coordinate transformation. See eq. (33) in Margolin et al. (2017)
+    chi = 2 * exp(y / (2 * l()))
+
     w = 1 + 1 / (2 * chi^2) * (1 - sqrt(1 + 2 * chi^2))
 
-    rho = rho_0 / w
-    u = v * (1 - w)
-    p = p_0 * 1 / w * (1 + (gamma - 1) / 2 * Ma^2 * (1 - w^2))
+    rho = rho_0() / w
+    u = v() * (1 - w)
+    p = p_0() * 1 / w * (1 + (gamma() - 1) / 2 * Ma()^2 * (1 - w^2))
 
     return prim2cons(SVector(rho, u, p), equations)
 end
+initial_condition = initial_condition_viscous_shock
 
-equations = CompressibleEulerEquations1D(gamma)
-equations_parabolic = CompressibleNavierStokesDiffusion1D(equations, mu = mu(),
+###############################################################################
+# semidiscretization of the ideal compressible Navier-Stokes equations
+
+equations = CompressibleEulerEquations1D(gamma())
+equations_parabolic = CompressibleNavierStokesDiffusion1D(equations, mu = mu_bar(),
                                                           Prandtl = prandtl_number(),
-                                                          gradient_variables = GradientVariablesEntropy())
+                                                          gradient_variables = GradientVariablesPrimitive())
 
 solver = DGSEM(polydeg = 3, surface_flux = flux_hlle)
 
 coordinates_min = -domain_length / 2
 coordinates_max = domain_length / 2
 
-mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level = 4,
-                periodicity = false,
-                n_cells_max = 30_000)
+refinement_patches = ((type = "box", coordinates_min = (-1.5,),
+                       coordinates_max = (1.5,)),
+                      (type = "box", coordinates_min = (-1.0,),
+                       coordinates_max = (1.0,)))
 
-initial_condition = initial_condition_viscous_shock
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level = 3,
+                periodicity = false,
+                #refinement_patches = refinement_patches,
+                n_cells_max = 30_000)
 
 ### Inviscid boundary conditions ###
 
 # Prescribe pure influx based on initial conditions
-function boundary_condition_inflow(u_inner, orientation, normal_direction, x, t,
-                                   surface_flux_function, equations)
-    u_cons = initial_condition(x, t, equations)
-    flux = Trixi.flux(u_cons, normal_direction, equations)
+function boundary_condition_inflow(u_inner, orientation::Integer, normal_direction, x, t,
+                                   surface_flux_function,
+                                   equations::CompressibleEulerEquations1D)
+    u_cons = initial_condition_viscous_shock(x, t, equations)
+    flux = Trixi.flux(u_cons, orientation, equations)
 
     return flux
 end
 
 # Completely free outflow
-function boundary_condition_outflow(u_inner, orientation, normal_direction, x, t,
-                                    surface_flux_function, equations)
+function boundary_condition_outflow(u_inner, orientation::Integer, normal_direction, x, t,
+                                    surface_flux_function,
+                                    equations::CompressibleEulerEquations1D)
     # Calculate the boundary flux entirely from the internal solution state
-    flux = Trixi.flux(u_inner, normal_direction, equations)
+    flux = Trixi.flux(u_inner, orientation, equations)
 
     return flux
 end
@@ -121,15 +132,19 @@ boundary_conditions = (; x_neg = boundary_condition_inflow,
 
 ### Viscous boundary conditions ###
 # For the viscous BCs, we use the known analytical solution
-velocity_bc = NoSlip((x, t, equations) -> Trixi.velocity(initial_condition(x,
-                                                                           t,
-                                                                           equations),
-                                                         equations))
+velocity_bc = NoSlip() do x, t, equations_parabolic
+    Trixi.velocity(initial_condition_viscous_shock(x,
+                                                   t,
+                                                   equations_parabolic),
+                   equations_parabolic)
+end
 
-heat_bc = Isothermal((x, t, equations) -> Trixi.temperature(initial_condition(x,
-                                                                              t,
-                                                                              equations),
-                                                            equations_parabolic))
+heat_bc = Isothermal() do x, t, equations_parabolic
+    Trixi.temperature(initial_condition_viscous_shock(x,
+                                                      t,
+                                                      equations_parabolic),
+                      equations_parabolic)
+end
 
 boundary_condition_parabolic = BoundaryConditionNavierStokesWall(velocity_bc, heat_bc)
 
@@ -140,6 +155,26 @@ semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabol
                                              initial_condition, solver;
                                              boundary_conditions = (boundary_conditions,
                                                                     boundary_conditions_parabolic))
+
+
+#=                                                                    
+A = jacobian_ad_forward(semi)
+
+using LinearAlgebra
+Eigenvalues = eigvals(A)
+
+# Complex conjugate eigenvalues have same modulus
+Eigenvalues = Eigenvalues[imag(Eigenvalues) .>= 0]
+
+# Sometimes due to numerical issues some eigenvalues have positive real part, which is erronous (for hyperbolic eqs)
+Eigenvalues = Eigenvalues[real(Eigenvalues) .< 0]
+
+EigValsReal = real(Eigenvalues)
+EigValsImag = imag(Eigenvalues)
+
+plotdata = Plots.scatter(EigValsReal, EigValsImag, label = "Spectrum")
+display(plotdata)
+=#
 
 ###############################################################################
 # ODE solvers, callbacks etc.
@@ -160,8 +195,8 @@ callbacks = CallbackSet(summary_callback, alive_callback, analysis_callback)
 ###############################################################################
 # run the simulation
 
-time_int_tol = 1e-8
+time_int_tol = 1e-9
 sol = solve(ode, RDPK3SpFSAL49(); abstol = time_int_tol, reltol = time_int_tol,
-            dt = 1e-5, ode_default_options()..., callback = callbacks)
+            dt = 1e-4, ode_default_options()..., callback = callbacks)
 
 summary_callback() # print the timer summary
