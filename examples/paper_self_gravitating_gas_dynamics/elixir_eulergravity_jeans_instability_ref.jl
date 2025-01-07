7ca4cba97547edc97d08f987d876ebf24178b6f9
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-01-07 15:48:51 +0100

    Bring in Jeans and sole HypDiff tests

---
+++ examples/paper_self_gravitating_gas_dynamics/elixir_eulergravity_jeans_instability_ref.jl
@@ -0,0 +1,188 @@
+
+using OrdinaryDiffEq
+using Trixi
+
+"""
+    initial_condition_jeans_instability(x, t,
+                                        equations::Union{CompressibleEulerEquations2D,
+                                                         HyperbolicDiffusionEquations2D})
+
+The classical Jeans instability taken from
+- Michael Schlottke-Lakemper, Andrew R. Winters, Hendrik Ranocha, Gregor J. Gassner (2020)
+  A purely hyperbolic discontinuous Galerkin approach for self-gravitating gas dynamics
+  [arXiv: 2008.10593](https://arxiv.org/abs/2008.10593)
+- Dominik Derigs, Andrew R. Winters, Gregor J. Gassner, Stefanie Walch (2016)
+  A Novel High-Order, Entropy Stable, 3D AMR MHD Solver with Guaranteed Positive Pressure
+  [arXiv: 1605.03572](https://arxiv.org/abs/1605.03572)
+- Flash manual https://flash.rochester.edu/site/flashcode/user_support/flash_ug_devel/node189.html#SECTION010131000000000000000
+in CGS (centimeter, gram, second) units.
+"""
+function initial_condition_jeans_instability(x, t,
+                                             equations::CompressibleEulerEquations2D)
+    # Jeans gravitational instability test case
+    # see Derigs et al. https://arxiv.org/abs/1605.03572; Sec. 4.6
+    # OBS! this uses cgs (centimeter, gram, second) units
+    # periodic boundaries
+    # domain size [0,L]^2 depends on the wave number chosen for the perturbation
+    # OBS! Be very careful here L must be chosen such that problem is periodic
+    # typical final time is T = 5
+    # gamma = 5/3
+    dens0 = 1.5e7 # g/cm^3
+    pres0 = 1.5e7 # dyn/cm^2
+    delta0 = 1e-3
+    # set wave vector values for perturbation (units 1/cm)
+    # see FLASH manual: https://flash.rochester.edu/site/flashcode/user_support/flash_ug_devel/node189.html#SECTION010131000000000000000
+    kx = 2.0 * pi / 0.5 # 2π/λ_x, λ_x = 0.5
+    ky = 0.0   # 2π/λ_y, λ_y = 1e10
+    k_dot_x = kx * x[1] + ky * x[2]
+    # perturb density and pressure away from reference states ρ_0 and p_0
+    dens = dens0 * (1.0 + delta0 * cos(k_dot_x))                 # g/cm^3
+    pres = pres0 * (1.0 + equations.gamma * delta0 * cos(k_dot_x)) # dyn/cm^2
+    # flow starts as stationary
+    velx = 0.0 # cm/s
+    vely = 0.0 # cm/s
+    return prim2cons((dens, velx, vely, pres), equations)
+end
+
+function initial_condition_jeans_instability(x, t,
+                                             equations::HyperbolicDiffusionEquations2D)
+    # gravity equation: -Δϕ = -4πGρ
+    # Constants taken from the FLASH manual
+    # https://flash.rochester.edu/site/flashcode/user_support/flash_ug_devel/node189.html#SECTION010131000000000000000
+    rho0 = 1.5e7
+    delta0 = 1e-3
+
+    phi = rho0 * delta0 # constant background perturbation magnitude
+    q1 = 0.0
+    q2 = 0.0
+    return (phi, q1, q2)
+end
+
+initial_condition = initial_condition_jeans_instability
+
+###############################################################################
+# semidiscretization of the compressible Euler equations
+gamma = 5 / 3
+equations_euler = CompressibleEulerEquations2D(gamma)
+
+polydeg = 3
+solver_euler = DGSEM(polydeg, FluxHLL(min_max_speed_naive))
+
+coordinates_min = (0.0, 0.0)
+coordinates_max = (1.0, 1.0)
+
+refinement_patches = ((type = "box", coordinates_min = (0.25, 0.25),
+                       coordinates_max = (0.75, 0.75)),
+                      (type = "box", coordinates_min = (0.375, 0.375),
+                       coordinates_max = (0.625, 0.625)))
+
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level = 4,
+                refinement_patches = refinement_patches,
+                n_cells_max = 10_000)
+
+semi_euler = SemidiscretizationHyperbolic(mesh, equations_euler, initial_condition,
+                                          solver_euler)
+
+###############################################################################
+# semidiscretization of the hyperbolic diffusion equations
+equations_gravity = HyperbolicDiffusionEquations2D()
+
+solver_gravity = DGSEM(polydeg, flux_lax_friedrichs)
+
+semi_gravity = SemidiscretizationHyperbolic(mesh, equations_gravity, initial_condition,
+                                            solver_gravity,
+                                            source_terms = source_terms_harmonic)
+
+###############################################################################
+# combining both semidiscretizations for Euler + self-gravity
+#=
+parameters = ParametersEulerGravity(background_density = 1.5e7, # aka rho0
+                                    gravitational_constant = 6.674e-8, # aka G
+                                    cfl = 0.8,
+                                    resid_tol = 1.0e-4,
+                                    n_iterations_max = 1000,
+                                    timestep_gravity = timestep_gravity_carpenter_kennedy_erk54_2N!)
+
+semi = SemidiscretizationEulerGravity(semi_euler, semi_gravity, parameters)
+=#
+
+base_path = "/home/daniel/git/MA/EigenspectraGeneration/PERK4/EulerGravity/Jeans_Instab/"
+
+Stages_Gravity = [10, 7, 5]
+dtRatios = [1, 0.5, 0.25]
+cfl_gravity = 0.9 # Seems to be stability limit
+
+alg_gravity = Trixi.PairedExplicitRK4Multi(Stages_Gravity, base_path * "HypDiff/", dtRatios)
+
+parameters = ParametersEulerGravity(background_density = 1.5e7, # aka rho0
+                                    gravitational_constant = 6.674e-8, # aka G
+                                    cfl = cfl_gravity,
+                                    resid_tol = 1.0e-4, # 1.0e-4
+                                    n_iterations_max = 1000,
+                                    timestep_gravity = Trixi.timestep_gravity_PERK4_Multi!)
+
+semi = SemidiscretizationEulerGravity(semi_euler, semi_gravity, parameters, alg_gravity)
+
+###############################################################################
+# ODE solvers, callbacks etc.
+tspan = (0.0, 5.0)
+ode = semidiscretize(semi, tspan);
+
+summary_callback = SummaryCallback()
+
+# Use same CFL as in paper (maybe avoid overshoots)
+cfl_euler = 0.5 # Can stable run with cfl = 3.0
+stepsize_callback = StepsizeCallback(cfl = cfl_euler)
+
+analysis_interval = 6
+
+Trixi.pretty_form_utf(::Val{:energy_potential}) = "∑e_potential"
+Trixi.pretty_form_ascii(::Val{:energy_potential}) = "e_potential"
+
+function Trixi.analyze(::Val{:energy_potential}, du, u_euler, t,
+                       semi::SemidiscretizationEulerGravity)
+    u_gravity = Trixi.wrap_array(semi.cache.u_ode, semi.semi_gravity)
+
+    mesh, equations_euler, dg, cache = Trixi.mesh_equations_solver_cache(semi.semi_euler)
+    _, equations_gravity, _, _ = Trixi.mesh_equations_solver_cache(semi.semi_gravity)
+
+    e_potential = Trixi.integrate_via_indices(u_euler, mesh, equations_euler, dg, cache,
+                                              equations_gravity,
+                                              u_gravity) do u, i, j, element,
+                                                            equations_euler, dg,
+                                                            equations_gravity, u_gravity
+        u_euler_local = Trixi.get_node_vars(u_euler, equations_euler, dg, i, j, element)
+        u_gravity_local = Trixi.get_node_vars(u_gravity, equations_gravity, dg, i, j,
+                                              element)
+        # OBS! subtraction is specific to Jeans instability test where rho0 = 1.5e7
+        return (u_euler_local[1] - 1.5e7) * u_gravity_local[1]
+    end
+    return e_potential
+end
+
+analysis_callback = AnalysisCallback(semi_euler, interval = analysis_interval,
+                                     save_analysis = true,
+                                     analysis_errors = Symbol[],
+                                     analysis_integrals = (energy_total, # Pretty much only sanity check
+                                                           energy_kinetic,
+                                                           energy_internal,
+                                                           Val(:energy_potential)))
+
+callbacks = CallbackSet(summary_callback, stepsize_callback,
+                        analysis_callback)
+
+###############################################################################
+# run the simulation
+
+dtRatios = [1, 0.5, 0.25]
+Stages = [11, 7, 5]
+
+ode_algorithm = Trixi.PairedExplicitRK4Multi(Stages, base_path * "Euler_only/", dtRatios)
+
+sol = Trixi.solve(ode, ode_algorithm,
+                  dt = 42.0,
+                  save_everystep=false, callback=callbacks);
+
+summary_callback() # print the timer summary
+println("Number of gravity subcycles: ", semi.gravity_counter.ncalls_since_readout)
---
+++ examples/paper_self_gravitating_gas_dynamics/elixir_hypdiff_convergence_ref.jl
@@ -0,0 +1,85 @@
+
+using OrdinaryDiffEq
+using Trixi
+
+###############################################################################
+# semidiscretization of the hyperbolic diffusion equations
+
+equations = HyperbolicDiffusionEquations2D()
+
+initial_condition = initial_condition_poisson_nonperiodic
+# 1 => -x, 2 => +x, 3 => -y, 4 => +y as usual for orientations
+boundary_conditions = (x_neg = boundary_condition_poisson_nonperiodic,
+                       x_pos = boundary_condition_poisson_nonperiodic,
+                       y_neg = boundary_condition_periodic,
+                       y_pos = boundary_condition_periodic)
+
+polydeg = 3
+surface_flux = flux_lax_friedrichs
+solver = DGSEM(polydeg, surface_flux)
+
+coordinates_min = (0.0, 0.0)
+coordinates_max = (1.0, 1.0)
+
+refinement_patches = ((type = "box", coordinates_min = (0.25, 0.25),
+                       coordinates_max = (0.75, 0.75)),
+                      (type = "box", coordinates_min = (0.375, 0.375),
+                       coordinates_max = (0.625, 0.625)))
+
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level = 2,
+                refinement_patches = refinement_patches,
+                n_cells_max = 30_000,
+                periodicity = (false, true))
+
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
+                                    source_terms = source_terms_poisson_nonperiodic,
+                                    boundary_conditions = boundary_conditions)
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+tspan = (0.0, 5.0)
+ode = semidiscretize(semi, tspan);
+
+summary_callback = SummaryCallback()
+
+resid_tol = 1.0e-10
+steady_state_callback = SteadyStateCallback(abstol = resid_tol, reltol = 0.0)
+
+# E = 11
+cfl = 3.5
+
+# Multi, E = [11, 7, 5]
+cfl = 3.3
+
+stepsize_callback = StepsizeCallback(cfl = cfl)
+
+analysis_interval = 5000
+alive_callback = AliveCallback(analysis_interval = analysis_interval)
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval,
+                                     extra_analysis_integrals = (entropy, energy_total))
+
+callbacks = CallbackSet(summary_callback, steady_state_callback, stepsize_callback,
+                        analysis_callback, alive_callback)
+
+###############################################################################
+# run the simulation
+
+base_path = "/home/daniel/git/MA/EigenspectraGeneration/PERK4/EulerGravity/HypDiff_Convergence/"
+
+Stages = 11
+#ode_algorithm = PERK4(Stages, base_path)
+
+
+dtRatios = [1, 0.5, 0.25]
+Stages = [11, 7, 5]
+
+ode_algorithm = Trixi.PairedExplicitRK4Multi(Stages, base_path, dtRatios)
+
+
+sol = Trixi.solve(ode, ode_algorithm,
+                  dt = 42.0,
+                  save_everystep=false, callback=callbacks);
+
+summary_callback() # print the timer summary
