bbf922ff60ab1ae67bb2d987046aa53527a8fe70
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-01-24 00:43:07 +0100

    symm mesh

--- examples/p4est_2d_dgsem/elixir_navierstokes_vortex_street.jl
+++ examples/p4est_2d_dgsem/elixir_navierstokes_vortex_street.jl
@@ -5,58 +5,64 @@ using Trixi
 # Semidiscretization of the compressible Euler equations
 
 # Fluid parameters
-gamma() = 5 / 3
-prandtl_number() = 0.72
+const gamma = 5 / 3
+const prandtl_number = 0.72
 
 # Parameters for compressible von-Karman vortex street
-Re() = 500
-Ma() = 0.5f0
-D() = 1 # Diameter of the cylinder as in the mesh file
+const Re = 500
+const Ma = 0.5f0
+const D = 1 # Diameter of the cylinder as in the mesh file
 
 # Parameters that can be freely chosen
-v_in() = 1
-p_in() = 1
+const v_in = 1
+const p_in = 1
 
 # Parameters that follow from Reynolds and Mach number + adiabatic index gamma
-mu() = v_in() * D() / Re()
+const mu = v_in * D / Re
 
-c() = v_in() / Ma()
-p_over_rho() = c()^2 / gamma()
-rho_in() = p_in() / p_over_rho()
+const c = v_in / Ma
+const p_over_rho = c^2 / gamma
+const rho_in = p_in / p_over_rho
 
 # Equations for this configuration
-equations = CompressibleEulerEquations2D(gamma())
-equations_parabolic = CompressibleNavierStokesDiffusion2D(equations, mu = mu(),
-                                                          Prandtl = prandtl_number(),
+equations = CompressibleEulerEquations2D(gamma)
+equations_parabolic = CompressibleNavierStokesDiffusion2D(equations, mu = mu,
+                                                          Prandtl = prandtl_number,
                                                           gradient_variables = GradientVariablesPrimitive())
 
 # Freestream configuration
-@inline function initial_condition(x, t, equations)
-    rho = rho_in()
-    v1 = v_in()
+@inline function initial_condition(x, t, equations::CompressibleEulerEquations2D)
+    rho = rho_in
+    v1 = v_in
     v2 = 0.0
-    p = p_in()
+    p = p_in
 
     prim = SVector(rho, v1, v2, p)
     return prim2cons(prim, equations)
 end
 
-# Mesh which is refined around the cylinder and the wake region
-mesh_file = Trixi.download("https://gist.githubusercontent.com/DanielDoehring/7312faba9a50ef506b13f01716b4ec26/raw/8e68f9006e634905544207ca322bc0a03a9313ad/cylinder_vortex_street.inp",
+# Symmetric mesh which is refined around the cylinder and in the wake region
+mesh_file = Trixi.download("https://gist.githubusercontent.com/DanielDoehring/7312faba9a50ef506b13f01716b4ec26/raw/f08b4610491637d80947f1f2df483c81bd2cb071/cylinder_vortex_street.inp",
                            joinpath(@__DIR__, "cylinder_vortex_street.inp"))
 mesh = P4estMesh{2}(mesh_file)
 
 bc_freestream = BoundaryConditionDirichlet(initial_condition)
 
-# Boundary names follow from the mesh file
-boundary_conditions = Dict(:Bottom => bc_freestream,
-                           :Circle => boundary_condition_slip_wall,
+# Boundary names follow from the mesh file.
+# Since this mesh is been generated using the symmetry feature of 
+# HOHQMesh.jl (https://trixi-framework.github.io/HOHQMesh.jl/stable/tutorials/symmetric_mesh/)
+# the mirrored boundaries are named with a "_R" suffix.
+boundary_conditions = Dict(:Circle => boundary_condition_slip_wall, # top half of the cylinder
+                           :Circle_R => boundary_condition_slip_wall, # bottom half of the cylinder
                            :Top => bc_freestream,
+                           :Top_R => bc_freestream, # aka bottom
                            :Right => bc_freestream,
-                           :Left => bc_freestream)
+                           :Right_R => bc_freestream, 
+                           :Left => bc_freestream,
+                           :Left_R => bc_freestream)
 
 # Parabolic boundary conditions                            
-velocity_bc_free = NoSlip((x, t, equations) -> SVector(v_in(), 0))
+velocity_bc_free = NoSlip((x, t, equations) -> SVector(v_in, 0))
 # Use adiabatic also on the boundaries to "copy" temperature from the domain
 heat_bc_free = Adiabatic((x, t, equations) -> 0)
 boundary_condition_free = BoundaryConditionNavierStokesWall(velocity_bc_free, heat_bc_free)
@@ -66,7 +72,6 @@ heat_bc_cylinder = Adiabatic((x, t, equations) -> 0)
 boundary_condition_cylinder = BoundaryConditionNavierStokesWall(velocity_bc_cylinder,
                                                                 heat_bc_cylinder)
 
-# Boundary conditions for comparison with VRMHD                                                                
 @inline function boundary_condition_copy(flux_inner,
                                          u_inner,
                                          normal::AbstractVector,
@@ -84,13 +89,17 @@ end
                                          equations::CompressibleNavierStokesDiffusion2D{GradientVariablesPrimitive})
     return flux_inner
 end
-                                                               
-boundary_conditions_para = Dict(:Bottom => boundary_condition_copy,
-                                :Circle => boundary_condition_cylinder,
-                                :Top => boundary_condition_copy,
-                                :Right => boundary_condition_free,
-                                :Left => boundary_condition_free)
 
+boundary_conditions_para = Dict(:Circle => boundary_condition_cylinder, # top half of the cylinder
+                                :Circle_R => boundary_condition_cylinder, # bottom half of the cylinder
+
+                                :Top => boundary_condition_copy, #boundary_condition_free,
+                                :Top_R => boundary_condition_copy, #boundary_condition_free, # aka bottom
+
+                                :Right => boundary_condition_free,
+                                :Right_R => boundary_condition_free,
+                                :Left => boundary_condition_free,
+                                :Left_R => boundary_condition_free)
 # Standard DGSEM sufficient here
 solver = DGSEM(polydeg = 3, surface_flux = flux_hll)
 
@@ -125,7 +134,7 @@ callbacks = CallbackSet(summary_callback,
 ###############################################################################
 # run the simulation
 
-time_int_tol = 1e-6
+time_int_tol = 1e-7
 sol = solve(ode,
             # Moderate number of threads (e.g. 4) advisable to speed things up
             RDPK3SpFSAL49(thread = OrdinaryDiffEq.True());
