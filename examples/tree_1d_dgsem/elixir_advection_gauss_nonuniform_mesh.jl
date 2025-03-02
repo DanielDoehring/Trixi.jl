cbd3d6597b4b3d7949188c77486856e671183599
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-03-02 18:55:52 +0100

    towards plotting for time limiting figure

--- examples/tree_1d_dgsem/elixir_advection_gauss_nonuniform_mesh.jl
+++ examples/tree_1d_dgsem/elixir_advection_gauss_nonuniform_mesh.jl
@@ -39,7 +39,7 @@ analysis_callback = AnalysisCallback(semi, interval = 1,
                                      extra_analysis_errors = (:conservation_error,),
                                      extra_analysis_integrals = (Trixi.entropy_math,),
                                      #analysis_filename = "entropy_ER.dat",
-                                     analysis_filename = "entropy_standard.dat",
+                                     #analysis_filename = "entropy_standard.dat",
                                      save_analysis = true)
 cfl = 3.5 # [16, 8]
 #cfl = 5.5 # [32, 16]
@@ -75,15 +75,177 @@ sol = Trixi.solve(ode, ode_alg,
 # Print the timer summary
 summary_callback()
 
+###############################################################################
+
+# Compute node values for scatter plot in physical domain
+
+using LinearAlgebra
+
+function gauss_lobatto_nodes(cell_min, cell_max)
+    # Gauss-Lobatto nodes in reference coordinates [-1, 1]
+    ref_nodes = [-1, -1/sqrt(5), 1/sqrt(5), 1]
+    
+    # Map reference nodes to physical coordinates
+    cell_center = (cell_max + cell_min) / 2
+    cell_half_width = (cell_max - cell_min) / 2
+    physical_nodes = cell_center .+ cell_half_width .* ref_nodes
+    
+    return physical_nodes
+end
+
+# For plotting of the overlapping DG nodes
+function gauss_lobatto_nodes_inward(cell_min, cell_max)
+  inward = 0.2
+  ref_nodes = [-1 + inward, -1/sqrt(5), 1/sqrt(5), 1 - inward]
+  
+  # Map reference nodes to physical coordinates
+  cell_center = (cell_max + cell_min) / 2
+  cell_half_width = (cell_max - cell_min) / 2
+  physical_nodes = cell_center .+ cell_half_width .* ref_nodes
+  
+  return physical_nodes
+end
+
+function compute_all_nodes()
+    all_nodes = []
+    cell_min = -4.0
+
+    # First 6 cells of size 0.5
+    for i in 1:6
+        cell_max = cell_min + 0.5
+        #nodes = gauss_lobatto_nodes(cell_min, cell_max)
+        nodes = gauss_lobatto_nodes_inward(cell_min, cell_max)
+        append!(all_nodes, nodes)
+        cell_min = cell_max
+    end
+
+    # Next 8 cells of size 0.25
+    for i in 1:8
+        cell_max = cell_min + 0.25
+        #nodes = gauss_lobatto_nodes(cell_min, cell_max)
+        nodes = gauss_lobatto_nodes_inward(cell_min, cell_max)
+        append!(all_nodes, nodes)
+        cell_min = cell_max
+    end
+
+    # Last 6 cells of size 0.5
+    for i in 1:6
+        cell_max = cell_min + 0.5
+        #nodes = gauss_lobatto_nodes(cell_min, cell_max)
+        nodes = gauss_lobatto_nodes_inward(cell_min, cell_max)
+        append!(all_nodes, nodes)
+        cell_min = cell_max
+    end
+
+    return all_nodes
+end
+
+# Compute nodes for all cells
+all_nodes = compute_all_nodes()
+
 using Plots
 
 # TODO: Compute x values of the DG nodes - what to do with the values at interfaces? Average?
 
-scatter(sol.u[end]) # For plain method
+x_limits = [-2, 2]
+
+scatter(all_nodes, sol.u[end],
+        xlims = x_limits) # For plain method
+#scatter!(sol.u[end]) # For relaxed method
+
+function gauss_lobatto_nodes_inward(cell_min, cell_max)
+  inward = 0.2
+  ref_nodes = [-1 + inward, -1/sqrt(5), 1/sqrt(5), 1 - inward]
+  
+  # Map reference nodes to physical coordinates
+  cell_center = (cell_max + cell_min) / 2
+  cell_half_width = (cell_max - cell_min) / 2
+  physical_nodes = cell_center .+ cell_half_width .* ref_nodes
+  
+  return physical_nodes
+end
+
+function compute_all_nodes()
+    all_nodes = []
+    cell_min = -4.0
+
+    # First 6 cells of size 0.5
+    for i in 1:6
+        cell_max = cell_min + 0.5
+        #nodes = gauss_lobatto_nodes(cell_min, cell_max)
+        nodes = gauss_lobatto_nodes_inward(cell_min, cell_max)
+        append!(all_nodes, nodes)
+        cell_min = cell_max
+    end
+
+    # Next 8 cells of size 0.25
+    for i in 1:8
+        cell_max = cell_min + 0.25
+        #nodes = gauss_lobatto_nodes(cell_min, cell_max)
+        nodes = gauss_lobatto_nodes_inward(cell_min, cell_max)
+        append!(all_nodes, nodes)
+        cell_min = cell_max
+    end
+
+    # Last 6 cells of size 0.5
+    for i in 1:6
+        cell_max = cell_min + 0.5
+        #nodes = gauss_lobatto_nodes(cell_min, cell_max)
+        nodes = gauss_lobatto_nodes_inward(cell_min, cell_max)
+        append!(all_nodes, nodes)
+        cell_min = cell_max
+    end
+
+    return all_nodes
+end
+
+# Compute nodes for all cells
+all_nodes = compute_all_nodes()
+
+using Plots
+
+# TODO: Compute x values of the DG nodes - what to do with the values at interfaces? Average?
+
+x_limits = [-2, 2]
+
+scatter(all_nodes, sol.u[end],
+        xlims = x_limits) # For plain method
 #scatter!(sol.u[end]) # For relaxed method
 
 minimum(sol.u[end])
 
-plot(sol)
+
+#=
+plot(sol, 
+    title = "\$u (t_f ) \$", label = "P-ERRK",
+    linewidth = 3, color = RGB(0, 84 / 256, 159 / 256),
+    xticks = [-4, -2, -1, 0, 1, 2, 4],
+    guidefont = font("Computer Modern", 16), tickfont = font("Computer Modern", 14),
+    legend = true)
+
+plot!(sol, label = "P-ERK",
+    linewidth = 3, color = RGB(246 / 256, 169 / 256, 0),
+    legend = true)
+=#
+
 pd = PlotData1D(sol)
-plot!(getmesh(pd))
+plot!(getmesh(pd),
+      xlims = x_limits)
+#=
+plot(sol, 
+    title = "\$u (t_f ) \$", label = "P-ERRK",
+    linewidth = 3, color = RGB(0, 84 / 256, 159 / 256),
+    xticks = [-4, -2, -1, 0, 1, 2, 4],
+    guidefont = font("Computer Modern", 16), tickfont = font("Computer Modern", 14),
+    legend = true)
+
+plot!(sol, label = "P-ERK",
+    linewidth = 3, color = RGB(246 / 256, 169 / 256, 0),
+    legend = true)
+=#
+
+pd = PlotData1D(sol)
+plot!(getmesh(pd),
+      xlims = x_limits)
+
+minimum(sol.u[end])
