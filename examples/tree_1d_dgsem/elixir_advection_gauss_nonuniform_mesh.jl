d15c8c0b584c437cd604be0439c300a4db9c76ed
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-03-02 17:41:20 +0100

    revisit non-uniform mesh testcase

--- examples/tree_1d_dgsem/elixir_advection_gauss_nonuniform_mesh.jl
+++ examples/tree_1d_dgsem/elixir_advection_gauss_nonuniform_mesh.jl
@@ -8,12 +8,12 @@ using Trixi
 advection_velocity = 1.0
 equations = LinearScalarAdvectionEquation1D(advection_velocity)
 
-num_flux = flux_lax_friedrichs
 num_flux = flux_godunov
 solver = DGSEM(polydeg = 3, surface_flux = num_flux)
 
-coordinates_min = -5.0 # minimum coordinate
-coordinates_max = 5.0 # maximum coordinate
+coordinates_min = -4.0 # minimum coordinate
+coordinates_max = 4.0 # maximum coordinate
+length = coordinates_max - coordinates_min
 
 refinement_patches = ((type = "box", coordinates_min = (-1.0,),
                        coordinates_max = (1.0,)),)
@@ -29,20 +29,21 @@ semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_gauss,
 ###############################################################################
 # ODE solvers, callbacks etc.
 
-ode = semidiscretize(semi, (0.0, 11.0))
+t_end = 1.0
+t_end = length + 1
+ode = semidiscretize(semi, (0.0, t_end))
 
 summary_callback = SummaryCallback()
 
 analysis_callback = AnalysisCallback(semi, interval = 1,
                                      extra_analysis_errors = (:conservation_error,),
                                      extra_analysis_integrals = (Trixi.entropy_math,),
-                                     analysis_filename = "1D_Adv_NonUni_Std.dat",
-                                     #analysis_filename = "1D_Adv_NonUni_Rel.dat",
+                                     #analysis_filename = "entropy_ER.dat",
+                                     analysis_filename = "entropy_standard.dat",
                                      save_analysis = true)
 cfl = 3.5 # [16, 8]
 #cfl = 5.5 # [32, 16]
 
-#cfl = 2.0 # CarpenterKennedy2N54
 stepsize_callback = StepsizeCallback(cfl = cfl)
 
 callbacks = CallbackSet(summary_callback,
@@ -60,26 +61,29 @@ dtRatios = [1, 0.5]
 Stages = [16, 8]
 #Stages = [32, 16]
 
-relaxation_solver = Trixi.RelaxationSolverNewton(max_iterations = 8)
-#relaxation_solver = Trixi.RelaxationSolverBisection()
+relaxation_solver = Trixi.RelaxationSolverNewton(max_iterations = 10)
 
 ode_alg = Trixi.PairedExplicitRelaxationRK2Multi(Stages, path, dtRatios;
                                                  relaxation_solver = relaxation_solver)
 
-#ode_alg = Trixi.PairedExplicitRK2Multi(Stages, path, dtRatios)
+ode_alg = Trixi.PairedExplicitRK2Multi(Stages, path, dtRatios)
 
 sol = Trixi.solve(ode, ode_alg,
-                  dt = 0.25,
+                  dt = 0.2,
                   save_everystep = false, callback = callbacks);
-
-#=
-sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false),
-            dt = 42.0,
-            save_everystep = false, callback = callbacks);
-=#
 
 # Print the timer summary
 summary_callback()
 
-#scatter(sol.u[end])
-scatter!(sol.u[end])
+using Plots
+
+# TODO: Compute x values of the DG nodes - what to do with the values at interfaces? Average?
+
+scatter(sol.u[end]) # For plain method
+#scatter!(sol.u[end]) # For relaxed method
+
+minimum(sol.u[end])
+
+plot(sol)
+pd = PlotData1D(sol)
+plot!(getmesh(pd))
