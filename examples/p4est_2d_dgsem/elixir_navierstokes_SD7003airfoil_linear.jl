d66e9eb9d550ec8cf540e514e3f981d6078919ab
Daniel_Doehring <doehring@acom.rwth-aachen.de>
2025-05-24 21:11:01 +0200

    store sol

--- examples/p4est_2d_dgsem/elixir_navierstokes_SD7003airfoil_linear.jl
+++ examples/p4est_2d_dgsem/elixir_navierstokes_SD7003airfoil_linear.jl
@@ -83,8 +83,8 @@ semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabol
 
 tspan = (0.0, 30 * t_c) # Try to get into a state where initial pressure wave is gone
 
-ode = semidiscretize(semi, tspan)
-#ode = semidiscretize(semi, tspan; split_problem = false) # for multirate PERK
+#ode = semidiscretize(semi, tspan)
+ode = semidiscretize(semi, tspan; split_problem = false) # for multirate PERK
 
 #=
 # For PERK Multi coefficient measurements
@@ -132,11 +132,11 @@ cfl = 6.2 # PERK 4 Multi E = 5, ..., 14
 #cfl = 6.5 # PERK 4 Single 12
 
 cfl = 7.4 # PEERRK_4 Multi E = 5, ..., 14
-cfl = 7.6 # Single PERK 14
+#cfl = 7.6 # Single PERK 14
 
-cfl = 1.9 # R-RK44
-cfl = 2.5 # R-TS64
-cfl = 2.6 # R-CKL54
+#cfl = 1.9 # R-RK44
+#cfl = 2.5 # R-TS64
+#cfl = 2.6 # R-CKL54
 
 stepsize_callback = StepsizeCallback(cfl = cfl)
 
@@ -154,8 +154,8 @@ save_restart = SaveRestartCallback(interval = 1_000_000, # Only at end
 
 callbacks = CallbackSet(stepsize_callback, # For measurements: Fixed timestep (do not use this)
                         alive_callback, # Not needed for measurement run
-                        #save_solution, # For plotting during measurement run
-                        save_restart, # For restart with measurements
+                        save_solution, # For plotting during measurement run
+                        #save_restart, # For restart with measurements
                         analysis_callback,
                         summary_callback);
 
@@ -176,12 +176,12 @@ relaxation_solver = Trixi.RelaxationSolverNewton(max_iterations = 5, root_tol = 
 
 #ode_algorithm = Trixi.PairedExplicitRelaxationRK4(Stages[1], path; relaxation_solver = relaxation_solver)
 
-#ode_algorithm = Trixi.PairedExplicitRelaxationRK4Multi(Stages, path, dtRatios; relaxation_solver = relaxation_solver)
+ode_algorithm = Trixi.PairedExplicitRelaxationRK4Multi(Stages, path, dtRatios; relaxation_solver = relaxation_solver)
 #ode_algorithm = Trixi.PairedExplicitRK4Multi(Stages, path, dtRatios)
 
 #ode_algorithm = Trixi.RelaxationRK44(; relaxation_solver = relaxation_solver)
 #ode_algorithm = Trixi.RelaxationTS64(; relaxation_solver = relaxation_solver)
-ode_algorithm = Trixi.RelaxationCKL54(; relaxation_solver = relaxation_solver)
+#ode_algorithm = Trixi.RelaxationCKL54(; relaxation_solver = relaxation_solver)
 
 # For measurement run with fixed timestep
 dt = 1e-3 # PERK4, dt_c = 2e-4
