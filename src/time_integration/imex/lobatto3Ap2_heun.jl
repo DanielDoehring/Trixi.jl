399ad8a9e01418b3408684f69b4acfe8371843ed
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-08-01 10:40:22 +0200

    comment

--- src/time_integration/imex/lobatto3Ap2_heun.jl
+++ src/time_integration/imex/lobatto3Ap2_heun.jl
@@ -65,7 +65,7 @@ mutable struct LobattoIII3Ap2HeunIntegrator{RealT <: Real, uType, Params, Sol, F
 
     # For split problems
     du_para::uType # Additional storage for the split-part of the rhs! function
-    u_nonlin::uType # Additional storage for the intermediate u approximation
+    u_nonlin::uType # Additional storage for the intermediate u approximation in nonlinear solver
     k1::uType # Naive implementation: add register for k1
 end
 
--- src/time_integration/imex/midpoint_midpoint.jl
+++ src/time_integration/imex/midpoint_midpoint.jl
@@ -66,7 +66,7 @@ mutable struct MidpointMidpointIntegrator{RealT <: Real, uType, Params, Sol, F, 
 
     # For split problems solved with IMEX methods
     du_para::uType # Additional storage for the split-part of the rhs! function
-    u_nonlin::uType # Additional storage for the intermediate u approximation
+    u_nonlin::uType # Additional storage for the intermediate u approximation in nonlinear solver
 end
 
 function init(ode::ODEProblem, alg::IMEX_Midpoint_Midpoint;
