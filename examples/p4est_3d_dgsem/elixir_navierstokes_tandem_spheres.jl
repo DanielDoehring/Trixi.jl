ee8e95232ee9203c2c9d1a56ce005b974d5d6c42
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-09-26 10:57:27 +0200

    dimensionalize

--- examples/p4est_3d_dgsem/elixir_navierstokes_tandem_spheres.jl
+++ examples/p4est_3d_dgsem/elixir_navierstokes_tandem_spheres.jl
@@ -11,9 +11,16 @@ D = 1 # Follows from mesh
 
 Re_D = 3900
 
-# TODO: Dimensionalize?
-U() = 0.1
-rho_ref() = 1.4 # 1.255
+rho_ref() = 1.255 # kg/m^3
+R_specific_air() = 287.052874 # J/(kg K)
+T_ref() = 300 # K
+p_ref() = rho_ref() * R_specific_air() * T_ref() # Pa = N/m^2
+
+# Speed of sound reference state
+c_ref() = sqrt(gamma * p_ref()/rho_ref()) # m/s
+Ma_ref() = 0.1
+U() = Ma_ref() * c_ref() # m/s
+
 mu() = rho_ref() * D * U()/Re_D # TODO: Sutherlands law
 
 equations = CompressibleEulerEquations3D(gamma)
@@ -22,14 +29,14 @@ equations_parabolic = CompressibleNavierStokesDiffusion3D(equations, mu = mu(),
 
 @inline function initial_condition(x, t, equations)
     # set the freestream flow parameters
-    rho_freestream = rho_ref()
+    rho_freestream = 1.255
 
     # v_total = 0.1 = Mach (for c = 1)
-    v1 = U()
+    v1 = 34.72206893029273
     v2 = 0.0
     v3 = 0.0
 
-    p_freestream = 1.0
+    p_freestream = 108075.40706099998
 
     prim = SVector(rho_freestream, v1, v2, v3, p_freestream)
     return prim2cons(prim, equations)
@@ -38,6 +45,7 @@ end
 bc_farfield = BoundaryConditionDirichlet(initial_condition)
 
 polydeg = 2
+
 surface_flux = flux_hll
 surface_flux = flux_lax_friedrichs
 
@@ -62,6 +70,10 @@ boundary_conditions = Dict(:FrontSphere => boundary_condition_slip_wall,
                            :BackSphere => boundary_condition_slip_wall,
                            :FarField => bc_farfield)
 
+semi_hyp = SemidiscretizationHyperbolic(mesh, equations,
+                                        initial_condition, solver;
+                                        boundary_conditions = boundary_conditions)
+
 velocity_bc = NoSlip((x, t, equations) -> SVector(0.0, 0.0, 0.0))
 heat_bc = Adiabatic((x, t, equations) -> 0.0)
 bc_spheres = BoundaryConditionNavierStokesWall(velocity_bc, heat_bc)
@@ -75,10 +87,12 @@ semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabol
                                              boundary_conditions = (boundary_conditions,
                                                                     boundary_conditions_para))
 
-t_star_end = 1 # 100
+t_star_end = 50 # 100
 t_end = t_star_end * D/U()
 tspan = (0.0, t_end)
-ode = semidiscretize(semi, tspan)
+
+ode = semidiscretize(semi_hyp, tspan)
+#ode = semidiscretize(semi, tspan)
 
 ###############################################################################
 
@@ -88,7 +102,15 @@ summary_callback = SummaryCallback()
 analysis_interval = 500
 analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
 
-alive_callback = AliveCallback(alive_interval = 1)
+alive_callback = AliveCallback(alive_interval = 10)
+
+t_ramp_up() = 2e-3 # For dimensionalized units
+cfl_0() = 10.0
+cfl_max() = 17.0
+
+cfl(t) = min(cfl_max(), cfl_0() + t/t_ramp_up() * (cfl_max() - cfl_0()))
+
+stepsize_callback = StepsizeCallback(cfl = cfl)
 
 save_sol_interval = 500
 save_solution = SaveSolutionCallback(interval = save_sol_interval,
@@ -99,9 +121,38 @@ save_restart = SaveRestartCallback(interval = save_sol_interval)
 callbacks = CallbackSet(summary_callback,
                         alive_callback, 
                         analysis_callback,
+                        stepsize_callback,
                         #save_solution,
                         #save_restart
                         )
+
+###############################################################################
+
+Stages = reverse(collect(range(2, 15)))
+
+Stages = [15, 14, 13, 12, 11, 9, 8, 7, 5, 4, 3, 2]
+
+dtRatios = reverse([0.0717675685882568359375
+0.147277712821960449219
+0.235483050346374511719
+0.291365385055541992188
+0.44527530670166015625
+0.515322089195251464844
+0.579870939254760742188
+0.71626186370849609375
+0.790532231330871582031
+0.891929268836975097656
+0.974652767181396484375
+1.04339599609375
+] ./ 1.04339599609375)
+
+path_coeffs = "/home/daniel/Sciebo/Job/Doktorand/Content/Meshes/HighOrderCFDWorkshop/CS1/Spectra_Coeffs/hyp/k2_LLF_weakform/"
+
+ode_alg = Trixi.PairedExplicitRK2Multi(Stages, path_coeffs, dtRatios)
+
+sol = Trixi.solve(ode, ode_alg,
+                  dt = 42.0,
+                  save_everystep = false, callback = callbacks);
 
 ###############################################################################
 
