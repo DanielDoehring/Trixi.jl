78b4f510c8ba0aafb004910773931d590702b16e
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-01-04 00:19:59 +0100

    fmt

--- examples/dgmulti_2d/elixir_euler_kelvin_helmholtz_instability_adaptive_VI.jl
+++ examples/dgmulti_2d/elixir_euler_kelvin_helmholtz_instability_adaptive_VI.jl
@@ -59,10 +59,11 @@ analysis_interval = 250
 analysis_callback = AnalysisCallback(semi, interval = analysis_interval, uEltype = real(dg))
 save_solution = SaveSolutionCallback(interval = analysis_interval,
                                      solution_variables = cons2prim)
+
 callbacks = CallbackSet(summary_callback,
                         analysis_callback,
                         alive_callback,
-                        save_solution) # TODO: Trixi2Vtk does not to work here
+                        save_solution)
 
 ###############################################################################
 # run the simulation
